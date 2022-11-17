40796e3891a6f5b055a21c3ec5f8820997933fd9
KanonKC <kanonkce@gmail.com>
2022-11-17 18:06:24 +0700

    Get color value

--- api/google.py
+++ api/google.py
@@ -1,8 +1,10 @@
 import gspread
 
+auth = gspread.service_account(filename="./service-account.json")
+doc = auth.open_by_key("1LragT8P41KwjRvdixHr1i5e_XbvytzSH-yHiRckTg6o")
+
 def getSheetScore():
-    auth = gspread.service_account(filename="./service-account.json")
-    doc = auth.open("TestScore")
-    sheet = doc.worksheet('Sheet1')
-    result = sheet.get('A2:B6')
-    return [{'team':i[0],'score':int(i[1])} for i in result]
+    worksheet = doc.get_worksheet(0)
+    result = sorted(worksheet.get_all_records(),key=lambda item: item['Score'],reverse=True)
+    
+    return [{'team':i['Team'],'score':i['Score'],'color':i['Color']} for i in result]
