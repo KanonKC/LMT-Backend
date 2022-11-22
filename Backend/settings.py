46aba9f6d748107cba9ae71a23ada3fb5179a87c
KanonKC <kanonkce@gmail.com>
2022-11-22 16:21:20 +0700

    allow scoreboard from netlify

--- Backend/settings.py
+++ Backend/settings.py
@@ -40,6 +40,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://scoreboard-lmt.netlify.app"
 ]
 
 CORS_ALLOW_METHODS = [
